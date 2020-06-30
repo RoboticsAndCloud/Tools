c4f474883f1c4adb23e95c46e9edfc615a2fd1ac
RoboticsAndCloud <lonefeifei@126.com>
2020-06-30 16:07:03 -0500

    Update utils_linux.py
    add functions

--- utils_linux.py
+++ utils_linux.py
@@ -9,9 +9,60 @@ Authors: fei(fei.liang@okstate.edu)
 Date: 2020/05/25 17:23:06
 """
 
+import datetime
 import socket
 import subprocess
 import os
+
+"""
+Brief: get file count of a director
+
+Raises:
+     NotImplementedError
+     FileNotFoundError
+"""
+def get_file_count_of_dir(dir, prefix=''):
+    path = dir
+    count = 0
+    for fn in os.listdir(path):
+        if os.path.isfile(dir + '/' + fn):
+            if prefix != '':
+                if prefix in fn:
+                    count = count + 1
+            else:
+                count = count + 1
+        else:
+            print('fn:', fn)
+    # count = sum([len(files) for root, dirs, files in os.walk(dir)])
+    return count
+
+"""
+Brief: get file list of a director
+
+Raises:
+     NotImplementedError
+     FileNotFoundError
+"""
+def get_file_list_of_dir(dir, prefix=''):
+    path = dir
+    count = 0
+    file_list = []
+    for fn in os.listdir(path):
+        file_path = dir + '/' + fn
+        if os.path.isfile(file_path):
+            if prefix != '':
+                if prefix in fn:
+                    file_list.append(file_path)
+            else:
+                file_list.append(file_path)
+        else:
+            print('fn:', fn)
+
+    return file_list
+
+
+def getCurrentTimestamp():
+    return datetime.datetime.now().strftime('%Y%m%d%H%M%S')
 
 def isRunning(process_name):
     try:
